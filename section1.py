223d2b0df73f90cfb9d21bd1c24257d59f70d402
na-toyoshima <na.toyoshim@gmail.com>
2020-07-04 14:32:35 +0900

    first commit

---
+++ section1.py
@@ -0,0 +1,58 @@
+def dec2bin(target):
+    amari = []
+    
+    while target != 0:
+        amari.append(target % 2)
+        target = target // 2
+        
+    amari.reverse()
+    return amari
+
+def dec2hex(target):
+    amari = []
+    
+    while target != 0:
+        amari.append(target % 16)
+        target = target // 16
+        
+    for i in range(len(amari)):
+        if amari[i] == 10:
+            amari[i] = 'A'
+        elif amari[i] == 11:
+            amari[i] = 'B'
+        elif amari[i] == 12:
+            amari[i] = 'C'
+        elif amari[i] == 13:
+            amari[i] = 'D'
+        elif amari[i] == 14:
+            amari[i] = 'E'
+        elif amari[i] == 15:
+            amari[i] = 'F'
+            
+    amari.reverse()
+    return amari
+
+def any2dec(target, m):
+    
+    n = len(target) -1
+    sum = 0
+    
+    for i in range(len(target)):
+        if target[i] == 'A':
+            num = 10
+        elif target[i] == 'B':
+            num = 11
+        elif target[i] == 'C':
+            num = 12
+        elif target[i] == 'D':
+            num = 13
+        elif target[i] == 'E':
+            num = 14
+        elif target[i] == 'F':
+            num = 15
+        else:
+            num = int(target[i])
+            
+        sum += (m ** n)*num
+        n -= 1
+    return sum
